7ae0c8f8eb5f847586b9c04b2408d21a33e89906
Robin Deits <robin.deits@gmail.com>
2015-02-15 14:48:36 -0500

    move contact into tests

--- puzzpy/test/contact.py
+++ puzzpy/test/contact.py
@@ -1,21 +1,22 @@
 # For use in playing Contact, to choose hard to guess words.
 # https://en.wikipedia.org/wiki/Botticelli_%28game%29#Games_similar_to_Botticelli
 #
-# From a trie on letters of English words, outputs words with maximal sum of 
+# From a trie on letters of English words, outputs words with maximal sum of
 # branching factor along the path from root to word terminal
 
 from puzzpy.wordsets.sowpods import SOWPODS
+from collections import defaultdict
 
-num_words = 100
+num_words = 10
 words = SOWPODS
 word_count = []
-dicty = {}
+
+Tree = lambda: defaultdict(lambda: Tree())
+dicty = Tree()
 
 for word in words:
     cur_dict = dicty
     for char in word:
-        if char not in cur_dict:
-            cur_dict[char] = {}
         cur_dict = cur_dict[char]
 
 for word in words:
